08bc2b12328c244cb62d0e2c476287bef6abf890
tarrus-001 <80381888+tarrus-001@users.noreply.github.com>
2024-12-17 13:27:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from './MovieCard';
 
 import './App.css';
 import SearchIcon from './search.svg';
-const API_URL = 'http://www.omdbapi.com/?apikey=a7aea976';
+const API_URL = 'https://www.omdbapi.com/?apikey=a7aea976';
 
 const App = () => {
     const [movies, setMovies] = useState([]);
